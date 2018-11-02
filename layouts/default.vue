682363c33a486019f1902705758d5bf261ecf643
Nasir Khan <virgodgreat@gmail.com>
2018-11-02 14:33:28 +0500

    Add pages and Update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <Topbar />
     <nuxt/>
   </div>
 </template>
+
+<script>
+import Topbar from '@/components/Topbar';
+export default {
+  components: {
+    Topbar
+  }
+}
+</script>
+
 
 <style>
 html {
